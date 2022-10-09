d5689a503f370bec92ddd60091feee423eb8d4ee
Kill00 <kill00@p.teamvery.dev>
2022-10-09 17:13:35 +0900

    0.3.2 Released - Add VersionManager

---
+++ Assets/Scripts/Management/VersionManager.cs
@@ -0,0 +1,15 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Management
+{
+public class VersionManager : MonoBehaviour
+{
+    public TextMeshProUGUI a;
+
+    public void Start()
+    {
+        a.text = $"Ver. {Application.version}\nBy - {Application.companyName}";
+    }
+}
+}
